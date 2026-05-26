9f881f216b51d314e1120caa1d7cf70b882df156
aksika <aksika@gmail.com>
2026-05-26 12:21:38 +0200

    #627 export loadKey from abmind index

--- src/index.ts
+++ src/index.ts
@@ -97,7 +97,7 @@ export { checkEmbeddingHealth } from "./embedding-health.js";
 export type { EmbeddingHealth } from "./embedding-health.js";
 
 // Crypto (secret vault)
-export { encrypt, decrypt, hasKey, getSecretsKey, getBackupKey, deriveKey, deriveFromPassphrase, writeKeyVerify, validateKey, loadKeyFromFile, _resetKeyCache } from "./crypto.js";
+export { encrypt, decrypt, hasKey, loadKey, getSecretsKey, getBackupKey, deriveKey, deriveFromPassphrase, writeKeyVerify, validateKey, loadKeyFromFile, _resetKeyCache } from "./crypto.js";
 export { loadNative } from "./native-loader.js";
 export { readFromKeyring, writeToKeyring } from "./keyring.js";
 export { metaGet, metaGetInt, metaSet, metaIncrement } from "./meta-store.js";
